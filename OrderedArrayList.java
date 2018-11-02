b117c91700b96a2a80181d7fe1736bf49034cc10
Jude-Rizzo <jprizzo2020@gmail.com>
2018-11-02 11:46:27 -0400

    end

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,10 +3,25 @@ public class OrderedArrayList<T> extends NoNullArrayList<T>{
   public OrderedArrayList(){
 
   }
-  
+
   public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
 
+  }
+
+  public boolean add(T mystery){
+    if (this.size() == 0){
+      return super.add(mystery)
+    } else{
+    for(int i = 0; i < this.size(); i++){
+      if(mystery.compareTo(this.get(i)) > 0 &&
+         mystery.compareTo(this.get(i+1) < 0)){
+             return super.add(i+1, mystery)
+           }
+    }
+
+    return super.add(mystery);
+  }
   }
 
 }
