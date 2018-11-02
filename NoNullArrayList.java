a26e5c904eb2fa44221fcdc58c2e9cbb412fe6a5
Jude-Rizzo <jprizzo2020@gmail.com>
2018-11-02 11:27:17 -0400

    Horray

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -7,4 +7,24 @@ public class NoNullArrayList<T> extends ArrayList<T>{
     super(startingCapacity);
 
   }
+
+  public T set(int index, T mystery){
+    if (mystery == null)
+      throw new IllegalArgumentException("No");
+    return super.set(index, mystery);
+
+  }
+
+  public boolean add(T mystery){
+    if (mystery == null)
+      throw new IllegalArgumentException("No");
+     return super.add(mystery);
+
+}
+public void add(int index, T mystery){
+  if (mystery == null)
+    throw new IllegalArgumentException("No");
+  super.add(index, mystery);
+
+}
 }
